3aa573b78bae319e65ed72008ee6a164a8dc226f
Fabrício Fraga Gimenez <36505529+fabriciofg@users.noreply.github.com>
2022-05-23 16:55:18 -0300

    novos exercícios

---
+++ Aula11/exe008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocudade do seu carro é ${vel}`);
+if(vel >= 60){ // Condição simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ Aula11/exe009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+console.log(`Vicendo em ${pais}`)
+
+if(pais == 'Brasil'){
+    console.log('Brasileiro')
+
+}else{
+    console.log('Estrangeiro')
+}
