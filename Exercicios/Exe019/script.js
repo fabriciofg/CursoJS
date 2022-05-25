9dd2ced62eb55fa5ec4fd6fcb1da6697dcdad388
Fabrício Fraga Gimenez <36505529+fabriciofg@users.noreply.github.com>
2022-05-25 16:54:34 -0300

    novo

--- Exercicios/Exe019/script.js
+++ Exercicios/Exe019/script.js
@@ -9,21 +9,62 @@ console.log(n.value)
 
 
 let res = document.getElementById('res')
+let tab = document.getElementById('tabela')
 
 let pessoa = {nome:'-', cpf:'-', nasc:'-', tel:'-', email:'-'}
 
+console.log(tab)
+
+limpa()
+
 
 function cadastrar(){    
-    pessoa.nome = n.value
-    pessoa.cpf = c.value
-    pessoa.nasc = na.value
-    pessoa.tel = t.value
-    pessoa.email = e.value
+    if(vazio() == true){
+        alert('Entre com os dados')
+        limpa()
+    }else{
+        pessoa.nome = n.value
+        pessoa.cpf = c.value
+        pessoa.nasc = na.value
+        pessoa.tel = t.value
+        pessoa.email = e.value
 
-    res.innerHTML += `<strong>Nome: </strong>${pessoa.nome}<br>`
-    res.innerHTML += `<strong>CPF: </strong>${pessoa.cpf}<br>`
-    res.innerHTML += `<strong>Nascimento: </strong>${pessoa.nasc}<br>`
-    res.innerHTML += `<strong>Telefone: </strong>${pessoa.tel}<br>`
-    res.innerHTML += `<strong>E-mail: </strong>${pessoa.email}<br>` 
+        if(VerificaCPF() == true){     
+            tab.innerHTML +=`
+            <tr>
+            <td>${pessoa.nome}</td>
+            <td>${pessoa.cpf}</td>
+            <td>${pessoa.nasc}</td>
+            <td>${pessoa.tel}</td>
+            <td>${pessoa.email}</td>
+            </tr>`
+        }else{
+            alert('CPF já cadastrado! Entre com outro cadastro')
+        }
+        limpa()
+    }
+}
+
+function vazio(){ // Verifica de os campos do formulário contem dados 
+    if(n.value == '' || c.value == '' || na.value == '' || t.value == '' || e.value == ''){
+        console.log('FALSE')
+        return true
+    }else{
+        console.log('FALSE')
+        return false
+    }
+}
+function limpa(){ // Limpa todos os campos do formulario
+    console.log('LIMPAR CAMPOS')   
+    n.value =''
+    c.value =''
+  //na.value =''
+    t.value =''
+    e.value =''
+
+    n.focus()
+}
+function VerificaCPF(){ // Verificar de CPF já esta cadastrado na tabela
+    return true
 }
 
